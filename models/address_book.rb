102f1f09e0cf7331504fcee94854e86d24c295cf
robthesman <robthesman@gmail.com>
2016-06-01 00:25:46 +0000

    Lesson 21 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,33 +1,42 @@
-require_relative 'entry.rb'
+require_relative 'entry'
+require 'csv'
 
 class AddressBook
      attr_reader :entries
-    
-    def initialize
+     
+     def initialize
         @entries = []
-    end
-    
-    def remove_entry(name, phone, email)
-        delete_entry = nil
-        
-        @entries.each do |entry|
-            if name == entry.name && phone == entry.phone_number && email == entry.email
-                delete_entry = entry
-            end
-        end
-            
-        @entries.delete(delete_entry)
-    end
+     end
+     
+     def remove_entry(name, phone, email)
+         delete_entry = nil
+         
+         @entries.each do |entry|
+             if name == entry.name && phone == entry.phone_number && email == entry.mail
+                 delete_entry = entry
+             end
+         end
+         
+         @entries.delete(delete_entry)
+     end
 
     def add_entry(name, phone_number, email)
         index = 0
-        @entries.each do |entry|
-            if name < entry.name
-                break
-            end
-            index += 1
-        end
         
-        @entries.insert(index, Entry.new(name, phone_number, email))
+       entries.insert(index, Entry.new(name, phone_number, email))
     end
+
+ 
+     def import_from_csv(file_name)
+     # Implementation goes here
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   # #8
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
+
+     end
+
 end
